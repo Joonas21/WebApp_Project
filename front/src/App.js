882cec3ef29f2f69f11ba84e5e8b8663ae4c7dae
Joonas21 <joonas.kukonlehto@gmail.com>
2021-12-18 19:43:00 +0200

    Hopefully final version

--- front/src/App.js
+++ front/src/App.js
@@ -19,7 +19,7 @@ function App() {
       .then(response => response.json())
       .then(json => setCodes(json))
 
-  }, [codes]) // Reaload the page everytime the codes changes
+  }, [codes]) // Reload the page everytime the codes changes
 
   return (
     <Router>
